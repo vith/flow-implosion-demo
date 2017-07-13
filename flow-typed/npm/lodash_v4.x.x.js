bb74c48db77536883c856d32e8823e1db0b2c659
Jason Papakostas <vithos@gmail.com>
2017-07-13 12:47:56 -0500

    remove irrelevant declaration

--- flow-typed/npm/lodash_v4.x.x.js
+++ flow-typed/npm/lodash_v4.x.x.js
@@ -1,8 +1,5 @@
 declare module 'lodash' {
 
-  declare type Predicate<T> =
-    | ((value: T, index: number, array: Array<T>) => any)
-
   declare class Lodash {
     // Array
     take<T>(array: ?Array<T>, n?: number): Array<T>;
